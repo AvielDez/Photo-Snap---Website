0132df45244e0c2e43f101f748ea44d3b3a5dd23
avieldez <avielhernandez.01@gmail.com>
2024-05-03 23:54:28 -0500

    added component generation

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.css';
+
+interface HeaderProps {
+  // Define your props here
+}
+
+export function Header(){
+  return (
+    <div className={styles.container}>
+      {/* Component content here */}
+    </div>
+  );
+}
---
+++ plopfile.js
@@ -0,0 +1,29 @@
+export default function (plop) {
+  plop.setGenerator("component", {
+    description: "component boiler plate",
+    prompts: [
+      {
+        type: "input",
+        name: "componentName",
+        message: "component name?",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "app/components/{{componentName}}/{{componentName}}.tsx",
+        templateFile: "plop-templates/component/component.hbs",
+      },
+      {
+        type: "add",
+        path: "app/components/{{componentName}}/{{componentName}}.module.css",
+        templateFile: "plop-templates/component/component.module.hbs",
+      },
+      {
+        type: "add",
+        path: "app/components/{{componentName}}/{{componentName}}.index.ts",
+        templateFile: "plop-templates/component/index.hbs",
+      },
+    ],
+  });
+}
