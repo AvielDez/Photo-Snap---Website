f4b01d0e7f114064aceebb70969feefc52d3ca64
avieldez <avielhernandez.01@gmail.com>
2024-05-07 10:14:26 -0500

    clean up

--- scripts/generate-components.mjs
+++ scripts/generate-components.mjs
@@ -1,9 +1,7 @@
-// scripts/generate-components.mjs
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Helper to resolve paths
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
