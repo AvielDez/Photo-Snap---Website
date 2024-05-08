d31932469c0e783f0b09ab4c97596a0f3a282326
avieldez <avielhernandez.01@gmail.com>
2024-05-08 07:23:27 -0500

    modified svg component generator and added generated to gitignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,7 +27,7 @@ module.exports = {
   overrides: [
     // React
     {
-      files: ['**/*.{js,jsx,ts,tsx}'],
+      files: ['**/*.{js,mjs,jsx,ts,tsx}'],
       plugins: ['react', 'jsx-a11y'],
       extends: [
         'plugin:react/recommended',
--- scripts/generate-components.mjs
+++ scripts/generate-components.mjs
@@ -1,6 +1,9 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { optimize } from 'svgo';
+import camelcase from 'camelcase';
+import prettier from 'prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,8 +13,41 @@ const svgsJsonPath = path.join(
   '../app/components/icons/svgs-to-generate.json',
 );
 const outputDirectory = path.join(__dirname, '../app/components/icons');
+const generatedDirectory = path.join(outputDirectory, 'generated');
 
-const generateComponent = (name, filePath) => `
+const convertToCamelCase = (svgContent) => {
+  return svgContent.replace(/(\s)([a-z0-9-]+)=/g, (_, prefix, attrName) => {
+    return `${prefix}${camelcase(attrName)}=`;
+  });
+};
+
+const extractInnerSvgContent = (svgContent) => {
+  const matches = svgContent.match(/<svg[^>]*>([\s\S]*?)<\/svg>/i);
+  return matches ? matches[1] : svgContent;
+};
+
+const formatWithPrettier = async (code) => {
+  const prettierConfigPath = path.join(__dirname, '../.prettierrc');
+  const prettierConfig = fs.existsSync(prettierConfigPath)
+    ? JSON.parse(fs.readFileSync(prettierConfigPath, 'utf8'))
+    : {};
+
+  return prettier.format(code, { ...prettierConfig, parser: 'typescript' });
+};
+
+const generateComponent = async (name, filePath) => {
+  let svgContent = fs.readFileSync(filePath, 'utf8');
+
+  // Optimize SVG using svgo
+  const optimizedSvg = optimize(svgContent, {
+    plugins: ['removeTitle', 'removeDesc'],
+  }).data;
+
+  // Convert attributes to camelCase
+  const camelCasedSvg = convertToCamelCase(optimizedSvg);
+  const innerSvgContent = extractInnerSvgContent(camelCasedSvg);
+
+  const componentCode = `
 import React from 'react';
 
 interface ${name}Props extends React.SVGAttributes<SVGElement> {
@@ -20,14 +56,16 @@ interface ${name}Props extends React.SVGAttributes<SVGElement> {
 
 const ${name}: React.FC<${name}Props> = ({ className, ...props }) => (
   <svg className={className} {...props}>
-    ${fs.readFileSync(filePath, 'utf8')}
+    ${innerSvgContent}
   </svg>
 );
 
 export default ${name};
-`;
+  `;
+  return await formatWithPrettier(componentCode);
+};
 
-const createIndexFile = (assets) => {
+const createIndexFile = async (assets) => {
   const indexContent = Object.keys(assets)
     .map((key) => {
       const componentName = key.replace(/-./g, (match) =>
@@ -39,29 +77,38 @@ const createIndexFile = (assets) => {
     })
     .join('\n');
 
-  fs.writeFileSync(path.join(outputDirectory, 'index.ts'), indexContent);
+  const formattedIndexContent = await formatWithPrettier(indexContent);
+
+  fs.writeFileSync(
+    path.join(outputDirectory, 'index.ts'),
+    formattedIndexContent,
+  );
 };
 
-const main = () => {
+const main = async () => {
   const { assets } = JSON.parse(fs.readFileSync(svgsJsonPath, 'utf8'));
 
   if (!fs.existsSync(outputDirectory)) {
     fs.mkdirSync(outputDirectory, { recursive: true });
   }
+  if (!fs.existsSync(generatedDirectory)) {
+    fs.mkdirSync(generatedDirectory, { recursive: true });
+  }
 
-  Object.entries(assets).forEach(([key, file]) => {
+  for (const [key, file] of Object.entries(assets)) {
     const componentName = key.replace(/-./g, (match) => match[1].toUpperCase());
     const pascalCaseName =
       componentName.charAt(0).toUpperCase() + componentName.slice(1);
-    const componentContent = generateComponent(
+    const componentContent = await generateComponent(
       pascalCaseName,
       path.join(__dirname, file),
     );
+
     fs.writeFileSync(
-      path.join(outputDirectory, `/generated/${pascalCaseName}.tsx`),
+      path.join(generatedDirectory, `${pascalCaseName}.tsx`),
       componentContent,
     );
-  });
+  }
 
   createIndexFile(assets);
 
